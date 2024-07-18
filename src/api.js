2793ab5a196905789e542a80bfe3bd9ac4751436
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-07-18 13:15:31 -0700

    added local storage for caching events offline

--- src/api.js
+++ src/api.js
@@ -38,7 +38,7 @@ export const getEvents = async () => {
   }
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
+    //NProgress.done();
     return events ? JSON.parse(events) : [];
   }
   const token = await getAccessToken();
@@ -49,7 +49,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
+      //NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
