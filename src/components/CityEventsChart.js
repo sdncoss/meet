ee45e581018e85fd4daaf91213b2a54287e1f32c
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-07-22 15:13:59 -0700

    changed Hook

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -11,18 +11,18 @@ import {
 const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split((/, | - /))[0]
+      const city = location.split(/, | - /)[0]
       return { city, count };
     })
     return data;
   };
-
-  useEffect(() => {
-    setData(getData());
-  }, [`${events}`]);
 
   return (
     <ResponsiveContainer width="99%" height={400}>
@@ -35,13 +35,19 @@ const CityEventsChart = ({ allLocations, events }) => {
         }}
       >
         <CartesianGrid />
-        <XAxis type="catagory" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
-        <YAxis type="number" dataKey="count" name="Number of Events" />
+        <XAxis
+          type="category" dataKey="city" name="City"
+          angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
+        <YAxis type="number" dataKey="count" name="number of events" allowDecimals={false} />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
     </ResponsiveContainer>
   );
 }
+
+
+
 
 export default CityEventsChart;
